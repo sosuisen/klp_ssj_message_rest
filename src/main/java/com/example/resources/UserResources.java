1820e33e50f07a3c8270031a88f8adee317f3ecb
sosuisen <hidekazu.kubota@gmail.com>
2023-07-27 16:33:35 +0900

    fix: for haihu

--- src/main/java/com/example/resources/UserResources.java
+++ src/main/java/com/example/resources/UserResources.java
@@ -5,26 +5,16 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 
 import com.example.auth.IdentityStoreConfig;
-import com.example.model.user.UserDTO;
 import com.example.model.user.UserRecord;
 import com.example.model.user.UsersDAO;
-import com.example.model.validator.CreateChecks;
 
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.security.enterprise.identitystore.Pbkdf2PasswordHash;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
-import jakarta.validation.groups.ConvertGroup;
-import jakarta.ws.rs.BeanParam;
-import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.NotFoundException;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
@@ -49,6 +39,10 @@ public class UserResources {
 		this.passwordHash = passwordHash;
 		passwordHash.initialize(IdentityStoreConfig.HASH_PARAMS);
 		this.req = req;
+		log.log(Level.INFO, "[user]%s [ip]%s [url]%s".formatted(
+				req.getRemoteUser(),
+				req.getRemoteAddr(),
+				req.getRequestURL().toString()));		
 	}
 	
 	@GET
@@ -60,67 +54,18 @@ public class UserResources {
 			throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
+
+	/**
+	 * 基本課題はこちらのコメントアウトを外して修正
 	@GET
 	@Path("{name}")
 	public UserRecord getUser(@PathParam("name") String name) {
-		try {
-			return usersDAO.get(name);
-		} catch (SQLException e) {
-			log.log(Level.SEVERE, "Error in getUser()", e);
-			throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
-		} catch (NotFoundException e) {
-			throw new WebApplicationException(Response.Status.NOT_FOUND);
-		}
+
+
 	}
+	*/
 	
-	@POST
-	public UserRecord postUser(@Valid @ConvertGroup(to = CreateChecks.class) @BeanParam UserDTO user) {
-		try {
-			var hash = passwordHash.generate(user.getPassword().toCharArray());
-			user.setPassword(hash);
-			return usersDAO.create(user);
-		} catch (SQLException e) {
-			log.log(Level.SEVERE, "Error in postUser()", e);
-			throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
-		}
-	}
+	// 以下は発展課題
+
 	
-	@PUT
-	@Path("{name}")
-	public UserRecord putUser(@PathParam("name") String name, @Valid @BeanParam UserDTO user) {
-		try {
-			user.setName(name);
-			usersDAO.update(user);
-			return usersDAO.get(name);
-		} catch (SQLException e) {
-			log.log(Level.SEVERE, "Error in putUser()", e);
-			throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
-		} catch (NotFoundException e) {
-			throw new WebApplicationException(Response.Status.NOT_FOUND);
-		}
-	}
-	
-	@DELETE
-	public void deleteAllUsers() {
-		try {
-			usersDAO.deleteAll();
-		} catch (SQLException e) {
-			log.log(Level.SEVERE, "Error in deleteAllUsers()", e);
-			throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
-		}
-	}
-	
-	@DELETE
-	@Path("{name}")
-	public void deleteUser(@PathParam("name") String name) {
-		try {
-			usersDAO.delete(name);
-		} catch (SQLException e) {
-			log.log(Level.SEVERE, "Error in deleteUser()", e);
-			throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
-		} catch (NotFoundException e) {
-			throw new WebApplicationException(Response.Status.NOT_FOUND);
-		}
-	}
 }
